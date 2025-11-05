a0014d5cf05730b4a577bce0d44827b66546a2e5
Menardo Madiumba <menardotera@gmail.com>
2025-11-05 08:56:27 +0100

    Base dashboard

---
+++ static/js/scripts.js
@@ -0,0 +1,15 @@
+
+const sidebare = document.querySelector('#sidebare');
+const container = document.querySelector(".main .container");
+const container__header = document.querySelector(".container__header");
+const container__content = document.querySelector(".container__content");
+const header = document.querySelector(".header");
+
+if (!document.body.contains(sidebare)) {
+    container.classList.add('container_flop')
+    container__header.classList.add("container_flop")
+}
+
+if (!document.body.contains(header)) {
+    container__content.classList.add('reset_h');
+}
