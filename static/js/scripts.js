99dc0ca698f9e3aaf6f94e30fce5b46c4b6d27fa
Menardo Madiumba <menardotera@gmail.com>
2025-11-05 09:55:29 +0100

    Base finaliser etape 2 ajout d'un hamb nav

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -5,6 +5,8 @@ const container__header = document.querySelector(".container__header");
 const container__content = document.querySelector(".container__content");
 const header = document.querySelector(".header");
 
+
+
 if (!document.body.contains(sidebare)) {
     container.classList.add('container_flop')
     container__header.classList.add("container_flop")
