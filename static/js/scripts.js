e090a427e05de24f5e9ff69e329a6190fa53a76c
Menardo Madiumba <menardotera@gmail.com>
2025-11-05 14:57:07 +0100

    Navbar close Hamburgeur logique

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,9 +1,11 @@
 
-const sidebare = document.querySelector('#sidebare');
-const container = document.querySelector(".main .container");
-const container__header = document.querySelector(".container__header");
+const sidebare   = document.querySelector('#sidebare');
+const container  = document.querySelector(".main .container");
+const container__header  = document.querySelector(".container__header");
 const container__content = document.querySelector(".container__content");
-const header = document.querySelector(".header");
+const header     = document.querySelector(".header");
+const overlyne   = document.querySelector(".overlyne");
+const hamb_close = document.querySelector(".hamb_close");
 
 
 
@@ -24,10 +26,29 @@ function Hamb(){
         e.preventDefault();
         if(sidebare.classList.contains('visible')){
             sidebare.classList.remove('visible');
+            overlyne.classList.remove('visible');
         }else{
-            sidebare.classList.add('visible')
+            sidebare.classList.add('visible');
+            overlyne.classList.add('visible');
         }
     });
+
+    // OVERLYNE
+
+    overlyne.addEventListener('click', (e) => {
+        e.preventDefault();
+        sidebare.classList.remove('visible');
+        overlyne.classList.remove('visible');
+    });
+
+    // CLOSE NAV
+
+    hamb_close.addEventListener('click', (e) => {
+        e.preventDefault();
+        sidebare.classList.remove('visible');
+        overlyne.classList.remove('visible');
+    });
+
 
 }
 
@@ -36,6 +57,7 @@ Hamb()
 function checkScreenSize() {
     if (window.innerWidth > 768) {
        sidebare.classList.remove('visible');
+       overlyne.classList.remove('visible');
     } 
 }
 
